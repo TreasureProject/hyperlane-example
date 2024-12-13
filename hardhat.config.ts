8830489974c4e8ad8530aa8d56e1ec345c60a8a7
mulf <mail@mulf.wtf>
2024-12-13 16:44:11 -0600

    chore: Configure Hardhat with Arbitrum network and Solidity optimizer

---
+++ hardhat.config.ts
@@ -0,0 +1,33 @@
+import "dotenv/config";
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-ethers";
+import "@nomicfoundation/hardhat-toolbox";
+
+if (typeof process.env.PRIVATE_KEY === "undefined") {
+    throw new Error("PRIVATE KEY REQUIRED");
+}
+
+if (typeof process.env.ARB_RPC_URL === "undefined") {
+    throw new Error("RPC URL required");
+}
+
+const config: HardhatUserConfig = {
+    solidity: {
+        version: "0.8.28",
+        settings: {
+            optimizer: {
+                enabled: true,
+                runs: 1000,
+            },
+        },
+    },
+    networks: {
+        arbitrum: {
+            url: process.env.ARB_RPC_URL,
+            accounts: [process.env.PRIVATE_KEY],
+            chainId: 42161,
+        },
+    },
+};
+
+export default config;

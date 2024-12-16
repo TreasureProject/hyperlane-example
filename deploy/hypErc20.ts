ab7d913ed1538987bc466e7359bad87514edc143
mulf <mail@mulf.wtf>
2024-12-16 15:09:50 -0600

    update deploy script

--- deploy/hypErc20.ts
+++ deploy/hypErc20.ts
@@ -1,21 +1,60 @@
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 import { DeployFunction } from "hardhat-deploy/types";
+import { NETWORK_CONFIG, TOKEN_CONFIG } from "../config";
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
-    const { deployments, getNamedAccounts } = hre;
+    const { deployments, getNamedAccounts, network } = hre;
     const { deploy } = deployments;
     const { deployer } = await getNamedAccounts();
 
-    // Sepolia Hyperlane Mailbox address
-    const MAILBOX_ADDRESS = "0xfFAEF09B3cd11D9b20d1a19bECca54EEC2884766";
+    const networkConfig = NETWORK_CONFIG.find((n) => n.name === network.name);
+    if (!networkConfig) {
+        throw new Error(`No config found for ${network.name}`);
+    }
 
-    await deploy("MyCustomHypERC20", {
+    const mailbox = await hre.ethers.getContractAt("IMailbox", networkConfig.mailbox);
+    const ism = networkConfig.customIsm || (await mailbox.defaultIsm());
+    const igp = networkConfig.customIgp || (await mailbox.defaultHook());
+
+    const result = await deploy("HypERC20", {
         from: deployer,
-        args: [18, MAILBOX_ADDRESS],
+        args: [TOKEN_CONFIG.decimals, networkConfig.mailbox],
         log: true,
-        waitConfirmations: 1,
+        waitConfirmations: 2,
     });
+
+    const token = await hre.ethers.getContractAt("HypERC20", result.address);
+
+    if (result.newlyDeployed) {
+        await token.initialize(
+            TOKEN_CONFIG.totalSupply,
+            TOKEN_CONFIG.name,
+            TOKEN_CONFIG.symbol,
+            igp,
+            ism,
+            deployer,
+        );
+
+        const gasConfigs = Object.entries(networkConfig.gasConfig.destinationGas).map(
+            ([domain, gas]) => ({
+                domain: parseInt(domain),
+                gas,
+            }),
+        );
+        await token.setDestinationGas(gasConfigs);
+
+        console.log(
+            "Save result:",
+            await deployments.save(`${TOKEN_CONFIG.name}-hlinfo`, {
+                abi: result.abi,
+                address: result.address,
+                peers: networkConfig.peers,
+            }),
+        );
+    }
 };
 
 func.tags = ["HypERC20"];
+func.dependencies = [];
+
 export default func;

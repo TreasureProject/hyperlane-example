5a744813e932c502ad145836b8c39ae58ea2dccc
mulf <mail@mulf.wtf>
2024-12-16 19:37:25 -0600

    update config file structure

--- config.ts
+++ config.ts
@@ -1,10 +1,13 @@
 interface HLConfig {
-    name: string;
-    chainId: number;
-    mailbox: string;
-    peers: {
-        chainId: number;
-        name: string;
+    srcChain: string;
+    pairing: {
+        contract: string;
+        srcAddr: string;
+        peers: {
+            contract: string;
+            destAddr: string;
+            chainId: number;
+        }[];
     }[];
     gasConfig: {
         destinationGas: {
@@ -16,43 +19,53 @@ interface HLConfig {
     customIgp?: string;
 }
 
-export const TOKEN_CONFIG = {
-    name: "My Hyperlane Token",
-    symbol: "MHT",
-    decimals: 18,
-    totalSupply: "1000000000000000000000000", // 1 million
-};
-
-export const NETWORK_CONFIG: HLConfig[] = [
+export const TOKEN_MAPPING: HLConfig[] = [
     {
-        name: "sepolia",
-        chainId: 11155111,
-        mailbox: "0xfFAEF09B3cd11D9b20d1a19bECca54EEC2884766",
-        peers: [
+        // must match network name in hardhat config
+        srcChain: "arbsepolia",
+
+        // all the contracts you want to enroll routers for on
+        // the specified srcChain
+        pairing: [
             {
-                name: "arbsepolia",
-                chainId: 421614,
+                // Contract name a router contract on the src chain
+                contract: "MyCustomHypERC20",
+                srcAddr: "0xd99D843C9Cab9FF5AabeC20aCCfd1D3b58656f31",
+                // cross chain peers
+                peers: [
+                    {
+                        contract: "MyCustomHypERC20",
+                        destAddr: "0xD7dB5038fb3a06A3bfeD6bab3b7E526d1f241277",
+                        chainId: 11155111,
+                    },
+                ],
+            },
+        ],
+        // used in the deploy process to define gas limit
+        gasConfig: {
+            destinationGas: {
+                11155111: 200000,
+            },
+        },
+    },
+    {
+        srcChain: "sepolia",
+        pairing: [
+            {
+                contract: "MyCustomHypERC20",
+                srcAddr: "0xD7dB5038fb3a06A3bfeD6bab3b7E526d1f241277",
+                peers: [
+                    {
+                        contract: "MyCustomHypERC20",
+                        destAddr: "0xd99D843C9Cab9FF5AabeC20aCCfd1D3b58656f31",
+                        chainId: 421614,
+                    },
+                ],
             },
         ],
         gasConfig: {
             destinationGas: {
                 421614: 500000,
-            },
-        },
-    },
-    {
-        name: "arbsepolia",
-        chainId: 421614,
-        mailbox: "0x598facE78a4302f11E3de0bee1894Da0b2Cb71F8",
-        peers: [
-            {
-                name: "sepolia",
-                chainId: 11155111,
-            },
-        ],
-        gasConfig: {
-            destinationGas: {
-                11155111: 200000,
             },
         },
     },

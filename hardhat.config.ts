3debdece7e018fca7f045e42f9405e98c7230cd6
mulf <mail@mulf.wtf>
2024-12-16 19:37:37 -0600

    add mailbox info to network config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -39,11 +39,13 @@ const config: HardhatUserConfig = {
             url: process.env.ARBSEP_RPC_URL,
             accounts: [process.env.PRIVATE_KEY],
             chainId: 421614,
+            lzMailbox: "0x598facE78a4302f11E3de0bee1894Da0b2Cb71F8",
         },
         sepolia: {
             url: process.env.SEP_RPC_URL,
             accounts: [process.env.PRIVATE_KEY],
             chainId: 11155111,
+            lzMailbox: "0xfFAEF09B3cd11D9b20d1a19bECca54EEC2884766",
         },
     },
 };

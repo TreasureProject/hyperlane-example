9b77d14741661a4cf8ed3f4a614f33e5b58a7e39
mulf <mail@mulf.wtf>
2024-12-16 19:36:58 -0600

    checkpoint transfer script

--- tasks/transfer.ts
+++ tasks/transfer.ts
@@ -1,34 +1,31 @@
 import { task } from "hardhat/config";
-import { IERC20 } from "../typechain-types";
-
+import { HypERC20__factory } from "../typechain-types";
 task("transfer", "Transfer tokens across chains")
     .addParam("destination", "Destination domain")
     .addParam("recipient", "Recipient address")
     .addParam("amount", "Amount to transfer")
     .setAction(async (taskArgs, { ethers, deployments }) => {
         const [signer] = await ethers.getSigners();
-        const deployment = await deployments.get("MyCustomHypERC20Collateral");
-        const tokenContract = await ethers.getContractAt(
-            "MyCustomHypERC20Collateral",
-            deployment.address,
-            signer,
-        );
+        const deployment = await deployments.get("MyCustomHypERC20");
+        const tokenContract = HypERC20__factory.connect(deployment.address, signer);
+        //
+        // console.log(`adding ${taskArgs.amount} to approval limit from underlying token`);
+        //
+        // const wrappedToken = await tokenContract.wrappedToken();
+        // const wrappedTokenInstance = (await ethers.getContractAt("IERC20", wrappedToken)) as IERC20;
+        //
 
-        console.log(`adding ${taskArgs.amount} to approval limit from underlying token`);
+        //        const approvalTx = await wrappedTokenInstance.approve(deployment.address, amt);
 
-        const wrappedToken = await tokenContract.wrappedToken();
-        const wrappedTokenInstance = (await ethers.getContractAt("IERC20", wrappedToken)) as IERC20;
+        //        approvalTx.wait(1);
 
-        const amt = ethers.parseUnits(taskArgs.amount);
-
-        const approvalTx = await wrappedTokenInstance.approve(deployment.address, amt);
-
-        approvalTx.wait(1);
+        const quote = await tokenContract.quoteGasPayment(parseInt(taskArgs.destination));
 
         const tx = await tokenContract.transferRemote(
             parseInt(taskArgs.destination),
             ethers.zeroPadValue(taskArgs.recipient, 32),
             ethers.parseUnits(taskArgs.amount, 18),
+            { value: quote },
         );
         await tx.wait();
         console.log(`Transferred ${taskArgs.amount} tokens to ${taskArgs.recipient}`);

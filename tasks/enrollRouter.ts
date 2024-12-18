028b5b6b891fd13761ba1b3f62fae6685b5456d2
mulf <mail@mulf.wtf>
2024-12-18 14:06:51 -0600

    change factory type

--- tasks/enrollRouter.ts
+++ tasks/enrollRouter.ts
@@ -21,9 +21,9 @@
 import { task } from "hardhat/config";
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 import { TOKEN_MAPPING } from "../config";
-import { HypERC20__factory } from "../typechain-types";
+import { TokenRouter__factory } from "../typechain-types";
 
-task("enroll-routers", "Enrolls remote routers for HypERC20 token").setAction(
+task("enroll-routers", "Enrolls remote routers for token").setAction(
     async (_, hre: HardhatRuntimeEnvironment) => {
         const { network, ethers } = hre;
         const [signer] = await ethers.getSigners();
@@ -38,30 +38,18 @@ task("enroll-routers", "Enrolls remote routers for HypERC20 token").setAction(
         }
 
         for (const { srcAddr, peers } of networkConfig.pairing) {
-            const token = HypERC20__factory.connect(srcAddr, signer);
+            const token = TokenRouter__factory.connect(srcAddr, signer);
 
             console.log("enrolling router");
             for (const peer of peers) {
                 console.log(peer);
-                await token.enrollRemoteRouter(
+                const enroll = await token.enrollRemoteRouter(
                     peer.chainId,
                     ethers.zeroPadValue(peer.destAddr, 32),
                 );
+                await enroll.wait(3);
                 console.log(peer.contract, "enrolled");
             }
         }
-        // for (const contract of networkConfig.contracts) {
-        //     try {
-        //         const peerDeployment = await deployments.get(`${contract.name}`);
-        //
-        //         await contract.enrollRemoteRouter(peer.chainId, peerDeployment.address);
-        //
-        //         console.log(`Enrolled ${peer.name} router for ${network.name} token`);
-        //     } catch (error) {
-        //         console.warn(
-        //             `Warning: Could not enroll peer ${peer.name}. Has it been deployed yet?`,
-        //         );
-        //     }
-        // }
     },
 );

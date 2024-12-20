7f5c2409b0ac466ce360ee279004bf54563e6d4d
mulf <mail@mulf.wtf>
2024-12-19 18:08:59 -0600

    new config format for better perring

--- config.ts
+++ config.ts
@@ -1,44 +1,83 @@
-type ChainConfig = {
-    id: number;
-    name: string;
-};
-
 type RouterConfig = {
     contract: string;
-    addresses: { [chainName: string]: string };
-    peerMap: { [chainName: string]: string[] };
+    networks: {
+        [networkName: string]: {
+            address: string;
+            peers: {
+                networkName: string;
+                address: string;
+                hook?: string;
+                ism?: string;
+            }[];
+        };
+    };
 };
 
 type HyperlaneConfig = {
-    chains: { [chainName: string]: ChainConfig };
     routers: RouterConfig[];
     gasConfig: { [sourceChainId: number]: { [destChainId: number]: number } };
 };
 
-export const HYPERCHAIN_CONFIG: HyperlaneConfig = {
-    chains: {
-        arbsepolia: { id: 421614, name: "Arbitrum Sepolia" },
-        sepolia: { id: 11155111, name: "Ethereum Sepolia" },
-        // base: { id: 84531, name: "Base Goerli" },
-    },
+export const HYPERLANE_CONFIG: HyperlaneConfig = {
     routers: [
         {
             contract: "HypERC1155",
-            addresses: {
-                arbsepolia: "0x1cB4698421fD63601A0a2fAE4CcDb0fe293851aF",
-                sepolia: "0xf97A63C68aA33F5B19595F01428f5a13D9aab075",
-                //  base: "0x1234567890123456789012345678901234567890",
+            networks: {
+                // networks the above named contract is deployed on
+                sepolia: {
+                    //address of the contract on the specified network
+                    address: "0xb2B4bCEF2094c7a56f4C6B",
+
+                    // contract address and network (as defined in hardhat config)
+                    // you want to pair the top level contract with
+                    peers: [
+                        {
+                            networkName: "arbsepolia",
+                            address: "0x8614b2565b5454Dd4",
+                        },
+                    ],
+                },
             },
-            peerMap: {
-                arbsepolia: ["sepolia"],
-                sepolia: ["arbsepolia"],
-                // base: ["sepolia", "arbsepolia"],
+        },
+        {
+            contract: "HypERC1155Collateral",
+            networks: {
+                arbsepolia: {
+                    address: "0x8614b2565b5454Dd",
+                    peers: [
+                        {
+                            networkName: "sepolia",
+                            address: "0xb2B4bCEF2094c7a56f4C6",
+                        },
+                    ],
+                },
             },
         },
     ],
     gasConfig: {
-        421614: { 11155111: 500000, 84531: 300000 },
-        11155111: { 421614: 200000, 84531: 250000 },
-        // 84531: { 421614: 300000, 11155111: 250000 },
+        421614: { 11155111: 500000 },
+        11155111: { 421614: 200000 },
     },
 };
+// // Example config
+// export const HYPERLANE_CONFIG: HyperlaneConfig = {
+//     // ... existing config
+//     routers: [
+//         {
+//             contract: "HypERC1155",
+//             networks: {
+//                 arbsepolia: {
+//                     address: "0x8614b2565b5454Dd4C3EfDC8897978A4DFA544C6",
+//                     peers: [
+//                         {
+//                             networkName: "sepolia",
+//                             hook: "0x1234...",
+//                             interchainSecurityModule: "0x5678..."
+//                         }
+//                     ]
+//                 },
+//                 // ... other networks
+//             },
+//         },
+//     ],
+// };
